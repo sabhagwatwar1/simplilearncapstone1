7592dc8403eb8bf2871cb0b4ae46166d706c1687
Ram Bhagwatwar <42444632+sabhagwatwar1@users.noreply.github.com>
2022-09-29 22:47:47 +0530

    Add files via upload

---
+++ Back-End/foodbox/src/main/java/com/simplilearn/foodbox/FoodboxApplication.java
@@ -0,0 +1,12 @@
+package com.simplilearn.foodbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodboxApplication.class, args);
+	}
+}
